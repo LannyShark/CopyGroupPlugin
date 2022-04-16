4f83be850b4ac5632017534c3ffa2cbf9e270f90
Илья Пермиловский <lannyshark@ya.ru>
2022-04-16 13:54:17 +0300

    Добавлена центровка по комнате

--- CopyGroupPlugin/Class1.cs
+++ CopyGroupPlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -15,21 +16,86 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDocument = commandData.Application.ActiveUIDocument;
-            Document document = uiDocument.Document;
+            try
+            {
+                UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+                Document document = uiDocument.Document;
 
-            Reference reference = uiDocument.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");
-            Element element = document.GetElement(reference);
-            Group group = element as Group;
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDocument.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу объектов");
+                Element element = document.GetElement(reference);
+                Group group = element as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(document, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
 
-            XYZ point = uiDocument.Selection.PickPoint("Выберите точку вставки");
+                XYZ point = uiDocument.Selection.PickPoint("Выберите точку вставки");
+                Room insertRoom = GetRoomByPoint(document, point);
+                XYZ insertRoomCenter = GetElementCenter(insertRoom);
+                XYZ insertPoint = insertRoomCenter+offset;
 
-            Transaction transaction = new Transaction(document);
-            transaction.Start("Копирование группы объектов");
-            document.Create.PlaceGroup(point, group.GroupType);
-            transaction.Commit();
+                Transaction transaction = new Transaction(document);
+                transaction.Start("Копирование группы объектов");
+                document.Create.PlaceGroup(insertPoint, group.GroupType);
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
 
             return Result.Succeeded;
+        }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+
+        public Room GetRoomByPoint (Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element element in collector)
+            {
+                Room room = element as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
